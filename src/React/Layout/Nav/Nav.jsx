608139b1ecd694233b23855b4178555e1d1b79ca
Christina Li <christinalee.is@gmail.com>
2022-02-03 18:44:59 -0800

    Adding Hamburger Icon

--- src/React/Layout/Nav/Nav.jsx
+++ src/React/Layout/Nav/Nav.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
+/* Sripts ---------------------------*/
+import{defaultMediaQueries, useMediaQuery} from 'React/common/useMediaQuery.js';
+
+/* Components ---------------------------*/
 import MainMenu from './MainMenu.jsx';
+import Hamburger from './Hamburger.jsx';
 
 const Nav = () => {
 
+    const { media } = useMediaQuery();
     return (
         <NavStyled className='Nav'>
+            {
+                    !media.mdUp &&
+                    <Hamburger/>
+            }
         <MainMenu/>
         </NavStyled>
     );
