3480b1b921127af3b80b2145ba1e15da07333821
Christina Li <christinalee.is@gmail.com>
2022-02-10 20:33:47 -0800

    Adding Tabbed Components

---
+++ src/React/Welcome/Tabbed/Content.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Content = ({ chosenTab }) => {
+
+    return (
+        <ContentStyled className='Content'>
+            <div className="left">
+                <img src={ chosenTab.image } alt={ chosenTab.title } />
+            </div>
+            <div className="right">
+                <h3> { chosenTab.title }</h3>
+
+                <div dangerouslySetInnerHTML={ {__html:chosenTab.text } } />
+                <div>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Quibusdam, eum quos nobis natus quae quis quisquam magni aliquam 
+                    numquam, ex fugiat, maiores placeat iusto unde in doloribus possimus? 
+                    Dicta, eius.
+                </div>
+            </div>
+        </ContentStyled>
+    );
+}
+
+export default Content;
+
+const ContentStyled = styled.div`
+    display: flex;
+    padding: 20px;
+    background-color: teal;
+
+    .left{
+        flex: 1;
+        img{
+            width: 100%;
+        }
+    }
+
+    .right{
+        flex: 3;
+        margin-left: 20px;
+    }
+`;
