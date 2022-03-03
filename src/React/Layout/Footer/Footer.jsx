268e6da86fe57b300a2fdc0af451f0c358f98be8
Christina Li <christinalee.is@gmail.com>
2022-03-02 20:23:45 -0800

    Updating Footer

--- src/React/Layout/Footer/Footer.jsx
+++ src/React/Layout/Footer/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
 export default Footer;
 
 const FooterStyled = styled.footer`
-    background-color: #2e392b;
+    background-color: #66875d;
     text-align: center;
     padding: 20px 0px;
 `;
