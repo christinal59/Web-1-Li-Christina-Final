27ab64482318b95900114f337313f17892107618
Christina Li <christinalee.is@gmail.com>
2022-03-02 20:27:04 -0800

    Updating Header

--- src/React/Layout/Header/Header.jsx
+++ src/React/Layout/Header/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
 export default Header;
 
 const HeaderStyled = styled.header`
-background-color: teal;
+background-color: #aaffff;
 padding: 20px 0px;
 display: flex;
 justify-content: center;
